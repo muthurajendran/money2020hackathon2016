7e48f2db8b8defe245dc2a53be26eb073271e024
Muthu <raju.spm@gmail.com>
2016-08-29 10:19:12 -0700

    add feed api

--- app/v1/routes/feed.js
+++ app/v1/routes/feed.js
@@ -1,16 +1,26 @@
 'use strict';
 
 var geocoder = require('app/v1/middlewares/geocoder');
-
+var Photo = require('../models/photo');
 
 var object = {
   userFeed: function(req, res, next) {
-    // geocoder.reverse({lat: 34.049285, lon: -118.355264}).then(function(res) {
-    //   console.log(res[0].extra.neighborhood);
-    // })
-    // .catch(function(err) {
-    //   console.log(err);
-    // });
+    try {
+      Photo.find({
+        location: { $geoWithin: { $centerSphere: [ [ req.body.longitude, req.body.latitude ], 3 / 3963.2 ] } }
+      }).populate(['reactions.sad','reactions.surprise','reactions.angry','reactions.lol','reactions.heart','reactions.thumbsUp'])
+      .then(function(photos) {
+        res.json({
+          success: true,
+          data: photos
+        });
+      })
+      .catch(function(err) {
+        return next({error: 'ERROR', message: err});
+      });
+    } catch (err) {
+      return next({error: 'ERROR', message: err});
+    }
   },
 
   getLocationName: function(req, res, next) {
