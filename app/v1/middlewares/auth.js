68e32e2e3d06cb3e144b5d5fb1d12747b418a92d
Muthu <raju.spm@gmail.com>
2016-10-05 00:38:34 -0700

    fixe

--- app/v1/middlewares/auth.js
+++ app/v1/middlewares/auth.js
@@ -4,13 +4,16 @@ var User = require('../models/user');
 
 function auth(req, res, next) {
   var token = req.body.access_token || req.query.access_token || req.headers['x-access-token'];
+  console.log('Token: ',token);
   if (!token) {
+    res.status(401);
     return res.json({ success: false, message: 'You need token to access this api.'});
   }
 
   User.findOne({accessToken: token})
   .then(function(user) {
     if (!user) {
+      res.status(401);
       return res.json({ success: false, message: 'Failed to authenticate token.'}); 
     } 
     req.isValid = true;
