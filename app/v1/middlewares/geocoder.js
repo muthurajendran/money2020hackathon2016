f39e6fb20fcab55502cf8bd10d5bb93b679f2728
Muthu <raju.spm@gmail.com>
2016-10-14 16:31:10 -0700

    location fix

--- app/v1/middlewares/geocoder.js
+++ app/v1/middlewares/geocoder.js
@@ -1,24 +1,87 @@
 'use strict';
 
-var NodeGeocoder = require('node-geocoder');
+// var NodeGeocoder = require('node-geocoder');
 var config = require('server/config/environment');
-var geocoder = NodeGeocoder(config.geocoder);
+// var geocoder = NodeGeocoder(config.geocoder);
+var googleMapsClient = require('@google/maps').createClient({
+  key: config.geocoder.apiKey
+});
+
 var Promise = require('promise');
+
+function processdata(data) {
+  return new Promise(function(resolve) {
+    var results = {
+      neighborhood: [],
+      city: []
+    };
+
+    for (var i = 0; i < data.length; i++) {
+      var components = data[i].address_components;
+      
+      for (var j = 0; j < components.length; j++) {
+        var component = components[j];
+        
+        if (component.types.indexOf('neighborhood') !== -1) {
+          results.neighborhood.push(component.long_name);
+        }
+        
+        if (component.types.indexOf('locality') !== -1) {
+          results.city.push(component.long_name);
+        }
+      }
+    }
+
+    if (i === data.length) {
+      resolve(results);
+    }
+  });
+}
 
 module.exports = {	
   getLocationName: function(latitude, longitude) {
+    console.log(latitude,longitude);
     return new Promise(function(resolve, reject) {
-      geocoder.reverse({lat: latitude, lon: longitude}).then(function(res) {
-        if (res[0].extra.neighborhood) {
-          console.log(res);
-          resolve(res[0].extra.neighborhood);
-        } else {
-          resolve(res[0].city);
+      googleMapsClient.reverseGeocode({
+        latlng: [latitude, longitude],
+        location_type: ['APPROXIMATE'],
+        result_type: ['neighborhood'],
+      }, function(err, response) {
+        if (!err) {
+          return processdata(response.json.results)
+          .then(function(results) {
+            if (results.neighborhood.length >= 1) {
+              resolve(results.neighborhood[0]);
+            } else {
+              resolve(results.city[0]);
+            }
+          });
         }
-      })
-      .catch(function(err) {
         reject(err);
       });
+      // googleMapsClient.reverseGeocode({
+      //   latlng: [latitude,longitude],
+      // }, function(err, response) {
+      //   if (!err) {
+      //     console,log(response.json.results);
+      //     resolve(response.json.results);
+      //   } else {
+      //     reject(err)
+      //   }
+      // });
+      // geocoder.reverse({lat: latitude, lon: longitude, zoom: '18', addressdetails: '1'}).then(function(res) {
+      //   resolve(res);
+      //   // if (res[0].extra.neighborhood) {
+      //   //   console.log(res);
+
+      //   //   // resolve(res[0].extra.neighborhood);
+      //   // } else {
+      //   //   resolve(res[0].city);
+      //   // }
+      // })
+      // .catch(function(err) {
+      //   reject(err);
+      // });
     });
   }
 };
